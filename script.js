7e7294ae2ebeeb531fe1b6209657f34410dc091e
Michal Nowak <michalnowak.contact@gmail.com>
2016-01-17 17:21:08 +0100

    zmiana 5

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function log (text) {
-	document.getElementsByTagName('textarea')[0].innerText=text;
+	document.getElementsByTagName('textarea')[0].value += text + "\n";
 }
+log("zzz");
+log("vvv");
